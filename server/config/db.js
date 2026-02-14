155ce5bdc9407b4788cf2094226ee8e47e61e9ab
christian7dev <christianmisikir@gmail.com>
2026-02-14 15:24:45 +0300

    updated backend

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 // db.js
+require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Atlas connected successfully! 🎉');
+    console.log('MongoDB Atlas connected successfully!');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1); // Exit process if connection fails (good for dev)
